09f7dac1064cc0444824e76d9ab0947186740c5a
Itai Danielli <Danielli.Itai@gmail.com>
2019-04-03 11:42:57 +0300

    using config file

--- Crawler.py
+++ Crawler.py
@@ -1,12 +1,16 @@
 from Base import Files
+from Base import Config
+
 from Source import research
 
 
-text = Files.TextRead(Files.WorkingDir()+'Config.ini');
+#text = Files.TextRead(Files.WorkingDir()+'Config.ini');
+ConfigFile:Config = Config.ConfigCls('Config.ini')
+text = ConfigFile.User()
 print(text);
 
 config:list = Files.IniRed(Files.WorkingDir()+'Config.ini');
-print(config['host']);
+#print(config['host']);
 
 
 research.crawl(5)
